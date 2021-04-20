32dcc8c6aade0d33e98bf4396506115fc831684f
agnihotrismita <81842452+agnihotrismita@users.noreply.github.com>
2021-04-21 10:28:01 +1200

    github jenkins inetgration
    github jenkins inetgration

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello
     public static void main(String args[])
     {
         System.out.println("hello");
+        System.ou.println("This is the testing integration between github and jenkins");
     }
 }
